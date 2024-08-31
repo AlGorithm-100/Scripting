6d72edc8a98fbc79586e3654c3b4170226d4799e
AlGorithm-100 <letsstartmusic@gmail.com>
2024-08-31 15:51:35 +0300

    Add files via upload

--- Destroy.cs
+++ Destroy.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class Destroy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        // Уничтожить объект в начале игры
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Выводить каждый кадр
-        print("crickets");
+        
     }
 }
